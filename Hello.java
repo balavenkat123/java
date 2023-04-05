dd5a37181f31e10c287bf290b8cfda49cd78b4c7
balavenkat123 <balavenkat17902@gmail.com>
2023-04-06 05:04:01 +0530

    java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        int a=250;float b=10.56f;byte c=125;
+        c=(byte)a;
+        System.out.println(c);
+    
+    
+    }
+
+}
